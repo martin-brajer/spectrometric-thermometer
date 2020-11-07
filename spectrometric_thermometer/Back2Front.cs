c32f0bf12e01e3b93093a53843eab7f51c450f4a
Martin Brajer <martin.brajer@seznam.cz>
2020-11-07 14:32:18 +0100

    test

--- spectrometric_thermometer/Back2Front.cs
+++ spectrometric_thermometer/Back2Front.cs
@@ -1,23 +1,30 @@
-﻿namespace spectrometric_thermometer
+﻿using System;
+using System.Drawing;
+
+namespace spectrometric_thermometer
 {
     public interface IBack2Front
     {
-        string My_msg { set; }
         string PidInfo { set; }
         string PidVoltage { set; }
+        KnownColor? LEDColor { set; }
+
+        void My_msg(string text);
     }
-    
+
     public class Back2Front : IBack2Front
     {
         private readonly Form_main front;
-
+        
         public Back2Front(Form_main front)
         {
             this.front = front;
         }
 
-        public string My_msg {set => front.My_msg(value); }
-        public string PidVoltage { set => front.tBoxVoltage.Text = value; }
+        public string PidVoltage { set => front.tBoxOutputVoltage.Text = value; }
         public string PidInfo { set => front.lblInfo.Text = value; }
+        public KnownColor? LEDColor { set => front.LEDColor = value; }
+
+        public void My_msg(string text) { front.My_msg(text); }
     }
 }
