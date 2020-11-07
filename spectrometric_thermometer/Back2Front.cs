86828b4fa54ffa9b288c31d63018926d07aceb8a
Martin Brajer <martin.brajer@seznam.cz>
2020-11-07 14:24:19 +0100

    Way to talk to GUI, moving out of Form

---
+++ spectrometric_thermometer/Back2Front.cs
@@ -0,0 +1,23 @@
+﻿namespace spectrometric_thermometer
+{
+    public interface IBack2Front
+    {
+        string My_msg { set; }
+        string PidInfo { set; }
+        string PidVoltage { set; }
+    }
+    
+    public class Back2Front : IBack2Front
+    {
+        private readonly Form_main front;
+
+        public Back2Front(Form_main front)
+        {
+            this.front = front;
+        }
+
+        public string My_msg {set => front.My_msg(value); }
+        public string PidVoltage { set => front.tBoxVoltage.Text = value; }
+        public string PidInfo { set => front.lblInfo.Text = value; }
+    }
+}
